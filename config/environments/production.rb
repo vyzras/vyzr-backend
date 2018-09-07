b4891b1c939f27974b8fa552ab23acfe3a2c7c7e
=Umair <=>
2018-09-07 17:21:36 +0500

    changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,8 +45,6 @@ Rails.application.configure do
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
-
-  RAILS_DEFAULT_LOGGER = Logger.new('log/production.log')
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
