ef98df413fd1dc45ddc9124f4bca3f0227102710
=Umair <=>
2018-10-10 17:19:46 +0500

    changes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -56,7 +56,7 @@ module Api::V1
     def fetch_items(list,user)
       items =  list.items
       items.each do |i|
-        a = user.list.items.find_or_create_by(title: i.data["Title"].to_s, description:i.data["CaseDescription"].to_s, author_id:i.data["AuthorId"].to_s,editor_id:i.data["EditorId"].to_s,item_uri: i.data['__metadata']['uri'],complete_percentage: i.data["PercentComplete"], created_time: i.data["created"],updated_time: i.data["modified"])
+        a = user.list.items.find_or_create_by(title: i.data["Title"].to_s, description:i.data["CaseDescription"].to_s, author_id:i.data["AuthorId"].to_s,editor_id:i.data["EditorId"].to_s,item_uri: i.data['__metadata']['uri'],complete_percentage: i.data["PercentComplete"], created_time: i.data["Created"],updated_time: i.data["Modified"])
         if a.errors.any?
           puts a.errors.full_messages
         end
