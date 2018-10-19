9ce0f80fd20362c362409f47d11585bdae5a4ed8
=Umair <=>
2018-10-19 18:59:22 +0500

    chagnes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -17,18 +17,18 @@ module Api::V1
     end
 
     def sign_in
-      @user =  User.find_or_create_by(email:params[:users][:user_name])
-         @user.save!
          site_name =     params[:users][:server_url]
          a = site_name.split('.com/')
          sites =  Sharepoint::Site.new a[0]+ ".com", a[1]
-         sites.session.authenticate     params[:users][:user_name],  params[:users][:password]
+         data =  sites.session.authenticate     params[:users][:user_name],  params[:users][:password]
          list = sites.list(params[:users][:list_name])
-         @user.list_user(list)
-         @user.generate_token
-         @user.save!
-         @user.list.update_attributes(guid: list.guid)
-         render json: {success: true , data: { user: @user.as_json(:except => [:created_at, :updated_at,:api_key,:first_name,:last_name]) ,user_token: @user.user_tokens.last.token}}
+
+         if data.nil?
+           render json: {success: true }
+         end
+
+
+
        end
 
 
